5f0cf23c65ca8ea166285de6175647835548df67
Kevin Chen <gchen@Kevins-MacBook-Pro.local>
2017-12-23 16:20:32 -0600

    complete asset diagnosis API
    Signed-off-by: Kevin Chen <gchen@Kevins-MacBook-Pro.local>

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -39,6 +39,7 @@ router.use(function(req, res, next) {
 router.use('/api/rigs', require('./routes/drillingrig.js'));
 router.use('/api/assets', require('./routes/assets.js'));
 router.use('/api/maintenance', require('./routes/maintenance.js'));
+router.use('/api/diagnosis', require('./routes/diagnosis.js'));
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
