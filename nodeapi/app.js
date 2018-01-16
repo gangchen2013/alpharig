4bdb2e8e62b9a2b8b776b33bf1ca46c6bc9d5ddf
Kevin Chen <gchen@Kevins-MacBook-Pro.local>
2018-01-16 17:57:36 -0600

    Return 403 on unrecognized auth token
    Signed-off-by: Kevin Chen <gchen@Kevins-MacBook-Pro.local>

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -62,7 +62,11 @@ router.use(function(req, res, next) {
 		jwt.verify(token, config.secret, function(err, decoded) {
 			if (err) {
 				console.log(JSON.stringify(err));
-				return res.json({ success: false, message: 'Failed to authenticate token.' });
+				//return res.json({ success: false, message: 'Failed to authenticate token.' });
+				return res.status(403).send({
+					success: false,
+					message: 'Failed to authenticate token.'
+				});
 			} else {
 				// if everything is good, save to request for use in other routes
 				req.decoded = decoded;
